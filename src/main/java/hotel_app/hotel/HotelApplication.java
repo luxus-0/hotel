939cfe9ec3dedda67865ef232f91b8e3a4b6fce9
LENOVO <luxus0@gmail.com>
2020-10-22 12:33:53 +0200

    change

--- src/main/java/hotel_app/hotel/HotelApplication.java
+++ src/main/java/hotel_app/hotel/HotelApplication.java
@@ -1,15 +1,7 @@
 package hotel_app.hotel;
 
-import hotel_app.hotel.dto.AddressDTO;
-import hotel_app.hotel.dto.BookingDTO;
-import hotel_app.hotel.dto.CustomerDTO;
-import hotel_app.hotel.entity.Address;
-import hotel_app.hotel.entity.Booking;
-import hotel_app.hotel.entity.Customer;
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HotelApplication {
