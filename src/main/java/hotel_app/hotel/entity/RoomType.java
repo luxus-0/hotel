99e763f4d53c4609c18166c78330c84eeafd42a7
LENOVO <luxus0@gmail.com>
2020-10-24 00:00:40 +0200

    add enum RoomType

--- src/main/java/hotel_app/hotel/entity/RoomType.java
+++ src/main/java/hotel_app/hotel/entity/RoomType.java
@@ -1,51 +1,21 @@
 package hotel_app.hotel.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
+public enum RoomType {
+    Economy("ECONOMY"), EconomyPremium("ECONOMY-PREMIUM"), Business("BUSINESS"), Luxury("I CLASS");
 
+    private  String description;
+    private final String name;
 
-@Entity
-public class RoomType
-{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String Standard;
-    private Integer numberOfCustomer;
-    private double price;
-
-    public Long getId() {
-        return id;
+    RoomType(String name) {
+        this.name = name;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getDescription() {
+        return description;
     }
 
-    public String getStandard() {
-        return Standard;
-    }
-
-    public void setStandard(String standard) {
-        Standard = standard;
-    }
-
-    public Integer getNumberOfCustomer() {
-        return numberOfCustomer;
-    }
-
-    public void setNumberOfCustomer(Integer numberOfCustomer) {
-        this.numberOfCustomer = numberOfCustomer;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
+    public String getName() {
+        return name;
     }
 }
