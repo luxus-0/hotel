83223f00ac7b827af01f66b375e83aad8843b4ea
LENOVO <luxus0@gmail.com>
2020-10-25 18:57:25 +0100

    add Service

--- src/main/java/hotel_app/hotel/service/BookingDateService.java
+++ src/main/java/hotel_app/hotel/service/BookingDateService.java
@@ -1,4 +1,12 @@
 package hotel_app.hotel.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookingDateService {
+
+    public void validBookingDateCheckIn()
+    {
+        
+    }
 }
