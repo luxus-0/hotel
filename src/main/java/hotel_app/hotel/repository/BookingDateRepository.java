c7a55c6573fd947411e8bb516b69f8c5b7646343
LENOVO <luxus0@gmail.com>
2020-10-26 19:28:40 +0100

    add Repository

--- src/main/java/hotel_app/hotel/repository/BookingDateRepository.java
+++ src/main/java/hotel_app/hotel/repository/BookingDateRepository.java
@@ -1,7 +1,15 @@
 package hotel_app.hotel.repository;
 
 import hotel_app.hotel.entity.BookingDate;
+import hotel_app.hotel.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface BookingDateRepository extends JpaRepository<BookingDate,Long> {
+
+    @Query("SELECT b FROM BookingDate b WHERE b.checkInDate = ?1")
+    List<BookingDate> findByCheckInDate(LocalDate checkInDate);
 }
