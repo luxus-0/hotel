400c3b35065db4d9d215b20644debfe8057fe8d8
LENOVO <luxus0@gmail.com>
2020-10-27 18:01:11 +0100

    add new Query

--- src/main/java/hotel_app/hotel/repository/BookingDateRepository.java
+++ src/main/java/hotel_app/hotel/repository/BookingDateRepository.java
@@ -1,11 +1,11 @@
 package hotel_app.hotel.repository;
 
 import hotel_app.hotel.entity.BookingDate;
-import hotel_app.hotel.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface BookingDateRepository extends JpaRepository<BookingDate,Long> {
@@ -16,6 +16,14 @@ public interface BookingDateRepository extends JpaRepository<BookingDate,Long> {
     @Query("SELECT b FROM BookingDate b WHERE b.lateCheckOut = :lateCheckOut")
     boolean findByLateCheckOutDate(boolean lateCheckOut);
 
+    @Query("SELECT b FROM BookingDate b WHERE b.estimatedCheckInTime = :estimatedCheckInTime")
+    LocalTime findByEstimatedCheckInTime(LocalTime estimatedCheckInTime);
+
+    @Query("SELECT b FROM BookingDate b WHERE b.estimatedCheckOutTime = :estimatedCheckOutTime")
+    LocalTime findByEstimatedCheckOutTime(LocalTime estimatedCheckOutTime);
+
     @Query("SELECT b FROM BookingDate b WHERE b.policyAcknowledged = :policyAcknowledged")
     boolean findByPolicyAcknowledge(boolean policyAcknowledged);
+
+
 }
