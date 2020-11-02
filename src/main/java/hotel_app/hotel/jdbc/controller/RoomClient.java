821c4fa32ba3180122d9ea579f620b430967be16
LENOVO <luxus0@gmail.com>
2020-11-02 13:13:39 +0100

    modified client

--- src/main/java/hotel_app/hotel/jdbc/controller/RoomClient.java
+++ src/main/java/hotel_app/hotel/jdbc/controller/RoomClient.java
@@ -1,9 +1,16 @@
 package hotel_app.hotel.jdbc.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import hotel_app.hotel.entity.RoomType;
 import hotel_app.hotel.jdbc.command.SelectRoomQuery;
 import hotel_app.hotel.jdbc.model.Room;
+import hotel_app.hotel.jdbc.query.DeleteRoomQuery;
+import hotel_app.hotel.jdbc.query.InsertRoomQuery;
+import hotel_app.hotel.jdbc.query.UpdateRoomQuery;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -11,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 public class RoomClient {
 
     private final SelectRoomQuery selectRoomQuery;
+    private final InsertRoomQuery insertRoomQuery;
+    private final UpdateRoomQuery updateRoomQuery;
+    private final DeleteRoomQuery deleteRoomQuery;
 
 @GetMapping("/rooms")
 public String getRooms() throws JsonProcessingException {
@@ -48,10 +58,59 @@ public String getRoomByAvailable(@PathVariable("available") Boolean available) t
     }
 
 @PostMapping("/rooms")
-public void addRoom(@RequestBody Room room)
+public ResponseEntity<HttpStatus> addRoom(@RequestBody Room room)
 {
-
+    return insertRoomQuery.addRoom(room);
 }
+
+@PutMapping("/rooms/{id}")
+public void updateRoom(@RequestBody Room room,@PathVariable("id") Long id)
+{
+    updateRoomQuery.updateRoom(room,id);
+}
+
+@PutMapping("/rooms")
+public void deleteRoom(@RequestBody Room room,@PathVariable("id") Long id)
+{
+    deleteRoomQuery.deleteRoom(room,id);
+}
+
+@PutMapping("/rooms")
+public void deleteRoom(@PathVariable("id") Long id)
+{
+        deleteRoomQuery.deleteRoomById(id);
+}
+
+@PutMapping("/rooms/{beds}")
+public void deleteBeds(@PathVariable("beds") Integer beds) {
+       deleteRoomQuery.deleteRoombyBeds(beds);
+    }
+
+@PutMapping("/rooms/{price}")
+public void deletePrice(@PathVariable("price") Double price) {
+       deleteRoomQuery.deleteRoomByPrice(price);
+    }
+
+@PutMapping("/rooms/{priceForNight}")
+public void deletePriceForNight(@PathVariable("priceForNight") Double priceForNight)
+    {
+        deleteRoomQuery.deleteRoomByPriceForNight(priceForNight);
+    }
+
+@PutMapping("/rooms/{available}")
+public void deleteAvailable(@PathVariable("available") Boolean available) {
+       deleteRoomQuery.deleteRoomByAvailable(available);
+    }
+
+@PutMapping("/rooms/{personNumber}")
+public void deleteRoomByPersonNumber(@PathVariable("personNumber") Integer personNumber) {
+       deleteRoomQuery.deleteRoomByPersonNumber(personNumber);
+    }
+
+public void deleteRoomByRoomType(@PathVariable("roomType") RoomType roomType) {
+       deleteRoomQuery.deleteRoomByRoomType(roomType);
+    }
+
 
 
 }
