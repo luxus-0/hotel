f4fc64bcf00b617b69989acc996b5b9c84b3353c
LENOVO <luxus0@gmail.com>
2020-11-11 16:07:18 +0100

    Initial commit

---
+++ src/main/resources/db/migration/V1_1__insert_reservation_schema.sql
@@ -0,0 +1,8 @@
+INSERT INTO reservation (adultNumber,childrenNumber,extraBedNumber,isPayment)
+VALUES(2,1,1,true);
+
+INSERT INTO reservation (adultNumber,childrenNumber,extraBedNumber,isPayment)
+VALUES(3,4,1,true);
+
+INSERT INTO reservation (adultNumber,childrenNumber,extraBedNumber,isPayment)
+VALUES(4,5,1,true);
