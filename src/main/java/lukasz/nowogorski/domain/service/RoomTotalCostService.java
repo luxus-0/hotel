1baaf275160a2068971927cd503e5e15d277aeef
LENOVO <luxus0@gmail.com>
2020-11-13 20:36:16 +0100

    add Service

---
+++ src/main/java/lukasz/nowogorski/domain/service/RoomTotalCostService.java
@@ -0,0 +1,10 @@
+package lukasz.nowogorski.domain.service;
+
+import java.math.BigDecimal;
+
+public interface RoomTotalCostService {
+
+    BigDecimal getTotalCost();
+    void infoTotalCost();
+
+}
