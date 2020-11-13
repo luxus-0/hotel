123cf9a0eb51805b9d57184244e2c32465550e81
LENOVO <luxus0@gmail.com>
2020-11-13 20:36:27 +0100

    add Service impl

---
+++ src/main/java/lukasz/nowogorski/domain/service/impl/RoomTotalCostServiceImpl.java
@@ -0,0 +1,19 @@
+package lukasz.nowogorski.domain.service.impl;
+
+import lukasz.nowogorski.domain.service.RoomTotalCostService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class RoomTotalCostServiceImpl implements RoomTotalCostService {
+    @Override
+    public BigDecimal getTotalCost() {
+        return null;
+    }
+
+    @Override
+    public void infoTotalCost() {
+
+    }
+}
