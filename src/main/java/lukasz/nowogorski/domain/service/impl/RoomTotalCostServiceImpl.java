e86c0c0fcf312f23b103311349cd4b0a6db6c0cf
LENOVO <luxus0@gmail.com>
2020-11-14 16:02:21 +0100

    changed in Service impl

--- src/main/java/lukasz/nowogorski/domain/service/impl/RoomTotalCostServiceImpl.java
+++ src/main/java/lukasz/nowogorski/domain/service/impl/RoomTotalCostServiceImpl.java
@@ -9,22 +9,12 @@ import java.time.LocalDateTime;
 @Service
 public class RoomTotalCostServiceImpl implements RoomTotalCostService {
 
-    private final RoomRepository roomRepository;
-
-    public RoomTotalCostServiceImpl(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
-    }
 
     @Override
     public Float getTotalCost(Integer numberDays,Float priceForNight) {
         Integer checkIn = LocalDateTime.now().getDayOfMonth();
         Integer checkOut = LocalDateTime.now().plusDays(numberDays).getDayOfMonth();
         return priceForNight * (checkOut-checkIn);
-    }
-
-    @Override
-    public void infoTotalCost() {
-        
     }
 
 }
