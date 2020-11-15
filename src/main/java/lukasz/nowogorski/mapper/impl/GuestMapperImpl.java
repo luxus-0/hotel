1d36340fc323830c78a3d27dd96a713967b6ae80
LENOVO <luxus0@gmail.com>
2020-11-15 18:57:28 +0100

    add mapper impl

---
+++ src/main/java/lukasz/nowogorski/mapper/impl/GuestMapperImpl.java
@@ -0,0 +1,33 @@
+package lukasz.nowogorski.mapper.impl;
+
+import lukasz.nowogorski.domain.model.Guest;
+import lukasz.nowogorski.dto.GuestDto;
+import lukasz.nowogorski.mapper.GuestMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GuestMapperImpl implements GuestMapper {
+    @Override
+    public Guest guestDtoToGuest(GuestDto guestDto) {
+        return Guest.builder()
+                .id(guestDto.getId())
+                .name(guestDto.getName())
+                .secondName(guestDto.getSecondName())
+                .surname(guestDto.getSurname())
+                .gender(guestDto.getGender())
+                .email(guestDto.getEmail())
+                .build();
+    }
+
+    @Override
+    public GuestDto guestToGuestDto(Guest guest) {
+        return GuestDto.builder()
+                .id(guest.getId())
+                .name(guest.getName())
+                .secondName(guest.getSecondName())
+                .surname(guest.getSurname())
+                .gender(guest.getGender())
+                .email(guest.getEmail())
+                .build();
+    }
+}
