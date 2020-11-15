e1ef5a250b26492b7a4b1ddd4cbad7b7ab4710ae
LENOVO <luxus0@gmail.com>
2020-11-15 18:56:50 +0100

    add dto

---
+++ src/main/java/lukasz/nowogorski/dto/EmployeeDto.java
@@ -0,0 +1,24 @@
+package lukasz.nowogorski.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lukasz.nowogorski.domain.model.Gender;
+
+import javax.validation.constraints.Email;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmployeeDto {
+
+    private Long id;
+    private String name;
+    private String secondName;
+    private String surname;
+    private Gender gender;
+    @Email(message = "message = Invalid email")
+    private String email;
+}
