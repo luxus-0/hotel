d2716de61c49c7ee48cfb11224429d5075e3f0fd
LENOVO <luxus0@gmail.com>
2020-11-15 11:01:26 +0100

    changed in domain

--- src/main/java/lukasz/nowogorski/domain/model/Employee.java
+++ src/main/java/lukasz/nowogorski/domain/model/Employee.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Entity
@@ -29,7 +31,11 @@ public class Employee {
     private Long pesel;
     private String nationality;
     private String telephone;
+    @Email(message = "Invalid email")
     private String email;
+    @Length(min = 6, max = 15, message = "Invalid length password")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?\":{}|<>]-).{6,15}")
+    private String password;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dateOfBirth;
