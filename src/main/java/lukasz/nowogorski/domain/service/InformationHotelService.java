669c2874dc881d09c004dfa670cee4ceddb1733a
LENOVO <luxus0@gmail.com>
2020-11-17 12:59:33 +0100

    Not finished yet

--- src/main/java/lukasz/nowogorski/domain/service/InformationHotelService.java
+++ src/main/java/lukasz/nowogorski/domain/service/InformationHotelService.java
@@ -1,4 +1,17 @@
 package lukasz.nowogorski.domain.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class InformationHotelService {
+
+    public void infoHotel()
+    {
+
+    }
+
+    public void showPicture()
+    {
+
+    }
 }
