6e39e88845e220cb27384dc59d112a9159827cd1
LENOVO <luxus0@gmail.com>
2020-12-26 22:11:21 +0100

    Initial main class

--- src/main/java/lukasz/nowogorski/Reservations.java
+++ src/main/java/lukasz/nowogorski/Reservations.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelApplication {
+public class Reservations {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelApplication.class, args);
+		SpringApplication.run(Reservations.class, args);
 	}
 }
