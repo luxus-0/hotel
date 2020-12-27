a60e74b9eb41e6e4f5060c549167bba8cbe233a3
LENOVO <luxus0@gmail.com>
2020-12-27 21:36:52 +0100

    create enum entity class

---
+++ src/main/java/lukasz/nowogorski/flight/model/AircraftTypeInternational.java
@@ -0,0 +1,7 @@
+package lukasz.nowogorski.flight.model;
+
+public enum AircraftTypeInternational
+{
+    AIRBUS_A320,AIRBUS_A330,AIRBUS_A380,
+    BOEING_737,BOEING_747_JUMBO_JET,BOEING_777,BOEING787_DREAMLINER,
+}
---
+++ src/main/java/lukasz/nowogorski/flight/model/AircraftTypeRegional.java
@@ -0,0 +1,8 @@
+package lukasz.nowogorski.flight.model;
+
+public enum AircraftTypeRegional
+{
+    BOMBARDIER_CRJ_900,BOMBARDIER_DASH_8,
+    EMBRAER_175,
+    ATR_72
+}
