77f26e9c1834ef7c47253810439b2784624a69ce
LENOVO <luxus0@gmail.com>
2021-01-03 23:50:59 +0100

    initial commit

--- src/main/java/lukasz/nowogorski/flight/service/FlightDetail.java
+++ src/main/java/lukasz/nowogorski/flight/service/FlightDetail.java
@@ -1,14 +1,24 @@
 package lukasz.nowogorski.flight.service;
 
-import lukasz.nowogorski.flight.repository.FlightRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Objects;
 
 @Service
+@RequiredArgsConstructor
+@Log4j2
 public class FlightDetail {
 
 
-    public void getDetail()
+    public void getDetail(String info)
     {
+        List<String> detailFlight = List.of(info);
 
+            detailFlight.stream()
+                    .filter(Objects::isNull)
+                    .findAny()
+                    .orElseThrow();
+        }
     }
-}
