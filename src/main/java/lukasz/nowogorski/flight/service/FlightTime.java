b7cb81c1c82014e094538c86d91f30dae3e897d1
LENOVO <luxus0@gmail.com>
2021-01-03 20:20:47 +0100

    initial commit

--- src/main/java/lukasz/nowogorski/flight/service/FlightTime.java
+++ src/main/java/lukasz/nowogorski/flight/service/FlightTime.java
@@ -1,9 +1,31 @@
 package lukasz.nowogorski.flight.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import lukasz.nowogorski.flight.repository.FlightRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class FlightTime {
+
+    private final FlightRepository repository;
+    private final FlightCreator creator;
+
+    public void reservationTimePassenger(LocalTime reserve) {
+        if(repository.findFlightByDepartureTime(reserve).isEmpty())
+        {
+            repository.save(creator.createTimeDeparture(reserve));
+            log.info("Departure Time: " +reserve);
+        }
+        else {
+            Set<LocalTime> timeDepart = Set.of(LocalTime.now());
+            timeDepart.stream().filter(p -> p.getHour() > 0).findAny().orElseThrow(RuntimeException::new);
+        }
+
+    }
 }
